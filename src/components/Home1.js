960124a84deddb3ae44c8526d4db0b222dece70d
dhruv7200 <rohitgajera72@gmail.com>
2021-12-30 20:20:46 +0530

    30dec

---
+++ src/components/Home1.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import studentim from './StudentNetwork.png';
+// import './Home.css';
+export default function home1(){
+    return(
+        <div>
+            <img src="C:\Users\Icom\Documents\reactdemo3\src\components\StudentNetwork.png" className="lgimage" alt="Student Network"/>
+        </div>
+    )
+}
