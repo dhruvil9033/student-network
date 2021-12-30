5f899a3a074c463810d4c36414a62e7d91aea010
dhruv7200 <rohitgajera72@gmail.com>
2021-12-30 20:20:36 +0530

    30dec

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from './components/Login';
 import Register from './components/Register';
 import Forgotpw from './components/forgotpassword';
+import Home1 from './components/Home1';
 import studentim from './components/StudentNetwork.png';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
         <Router>
           <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-            <img src={studentim} className="lgimage"/>
-            <div className="container">
-              <Link className="navbar-brand" to={"/login"}>
 
+            <div className="container">
+              <div className="navbar-brand">
+              <Link  to={"/login"}>
+                <img src={studentim} className="lgimage"/>
               </Link>
+              </div>
               <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul className="navbar-nav ml-auto">
                   <li className="nav-item">
@@ -38,7 +50,9 @@ function App() {
                 <Route path="/login" component={ Login }/>
                 <Route path="/signup" component={ Register }/>
                 <Route path="/forgotpass" component={ Forgotpw }/>
+                <Route path="/home1" component={ Home1 }/>
               </Switch>
+              {/*<p>{!data ? "Loading...": data}</p>*/}
             </div>
           </div>
         </Router>
